051bc00a29e868d5c65bfb3fa925272713d87533
Lucy Xiang <sam.lucy.xiang@gmail.com>
2017-10-21 19:51:10 -0400

    Add files via upload

---
+++ src/main/java/com/dhirajkumarcoder/android/tinderpets/LoginActivity.java
@@ -0,0 +1,63 @@
+package com.dhirajkumarcoder.android.tinderpets;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+
+            FacebookSdk.sdkInitialize(getApplicationContext());
+            setContentView(R.layout.activity_login);
+
+
+
+            callbackManager = CallbackManager.Factory.create();
+
+            loginButton = (LoginButton) findViewById(R.id.button_facebook_login1);
+            loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                @Override
+                public void onSuccess(LoginResult loginResult) {
+                    goMainScreen();
+                }
+
+                @Override
+                public void onCancel() {
+                    Toast.makeText(getApplicationContext(), "login error", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onError(FacebookException error) {
+                    Toast.makeText(getApplicationContext(), "LoginError", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+    private void goMainScreen() {
+        Intent intent = new Intent(this, StartupActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int ResultCode, Intent data) {
+        super.onActivityResult(requestCode, ResultCode, data);
+        callbackManager.onActivityResult(requestCode, ResultCode, data);
+    }
+
+}
