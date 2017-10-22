53fde146ea0de6022078d6b4c4b2d22252301e6e
mdakram28 <mdakram28@gmail.com>
2017-10-21 19:49:29 -0700

    firebase

---
+++ app/src/main/java/com/dhirajkumarcoder/android/tinderpets/FirebaseUtil.java
@@ -0,0 +1,8 @@
+package com.dhirajkumarcoder.android.tinderpets;
+
+/**
+ * Created by srbhj on 21-10-2017.
+ */
+
+public class FirebaseUtil {
+}
---
+++ app/src/main/java/com/dhirajkumarcoder/android/tinderpets/models/Pet.java
@@ -0,0 +1,8 @@
+package com.dhirajkumarcoder.android.tinderpets.models;
+
+/**
+ * Created by srbhj on 21-10-2017.
+ */
+
+public class Pet {
+}
