5b14079790bbf02c6b20bdcfcf96a27a3d016f25
TheAlgo <dhirajkumar.jain2016@vitstudent.ac.in>
2017-10-22 08:05:10 +0530

    Form and fragments added

--- app/src/main/java/com/dhirajkumarcoder/android/tinderpets/ProfileFragment.java
+++ app/src/main/java/com/dhirajkumarcoder/android/tinderpets/ProfileFragment.java
@@ -1,16 +1,13 @@
 package com.dhirajkumarcoder.android.tinderpets;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends android.app.Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
