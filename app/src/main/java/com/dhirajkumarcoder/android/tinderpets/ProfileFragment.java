5032573932debd7e60de5f48c9fe30010e508c72
TheAlgo <dhirajkumar.jain2016@vitstudent.ac.in>
2017-10-22 14:40:57 +0530

    recycler view complwtw

--- app/src/main/java/com/dhirajkumarcoder/android/tinderpets/ProfileFragment.java
+++ app/src/main/java/com/dhirajkumarcoder/android/tinderpets/ProfileFragment.java
@@ -8,7 +8,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import com.dhirajkumarcoder.android.tinderpets.Model.UiModels.User;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -40,6 +40,7 @@ public class ProfileFragment extends android.app.Fragment implements View.OnClic
         View view =  inflater.inflate(R.layout.fragment_profile, container, false);
         ButterKnife.bind(this,view);
         btnAddPet.setOnClickListener(this);
+        name.setText(User.);
         return view;
     }
 
